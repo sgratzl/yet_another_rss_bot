e8cb744743b200ea2145a3b026b062b081821b23
Samuel Gratzl <samuel_gratzl@gmx.at>
2019-10-21 17:08:21 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Yet Another RSS Telegram Bot
+[![Github Actions][github-actions-image]][github-actions-url]
+
+[Yet Another RSS Telegram Bot](https://t.me/yet_another_rss_bot) is a simple Telegram bot that convert RSS messages to chat messages
+
+## Privacy
+
+
+## Commands
+
+
+## Building
+
+```sh
+npm install
+npm run build
+```
+
+[github-actions-image]: https://github.com/sgratzl/yet_another_rss_bot/workflows/nodeci/badge.svg
+[github-actions-url]: https://github.com/sgratzl/yet_another_rss_bot/actions
---
+++ api/bot.ts
@@ -0,0 +1,22 @@
+import {NowRequest, NowResponse} from '@now/node';
+import Telegraf, {Markup} from 'telegraf';
+import {ok} from './_internal/responses';
+import {toArgs} from './_internal/telegram';
+
+let serverUrl = '';
+
+const bot = new Telegraf(process.env.BOT_TOKEN!, {
+  username: 'yet_another_rss_bot'
+});
+
+bot.start((ctx) => {
+  ctx.reply('This bot forwards RSS updates as chat messages');
+});
+
+export default async function handle(req: NowRequest, res: NowResponse) {
+  serverUrl = `https://${req.headers.host}/api`;
+
+  await bot.handleUpdate(req.body);
+
+  return ok(res);
+}
